f7cb8ac5f8f21cff23169f9545a08b93fe23b67c
Andy Deng <andydengaroo@gmail.com>
2024-12-17 16:26:47 -0500

    Update metadata in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,34 @@ const geistMono = Geist_Mono({
   subsets: ['latin']
 });
 
+const baseUrl = 'https://andydeng.me';
+
 export const metadata: Metadata = {
-  title: 'Andy Deng',
-  description: 'Andy Deng'
+  metadataBase: new URL(baseUrl),
+  title: {
+    default: 'Andy Deng',
+    template: '%s | Andy Deng'
+  },
+  description: "Andy Deng's Portfolio",
+  openGraph: {
+    title: 'Andy Deng Portfolio',
+    description: "Andy Deng's Portfolio",
+    url: baseUrl,
+    siteName: 'Andy Deng Portfolio',
+    locale: 'en_US',
+    type: 'website'
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1
+    }
+  }
 };
 
 export default function RootLayout({
